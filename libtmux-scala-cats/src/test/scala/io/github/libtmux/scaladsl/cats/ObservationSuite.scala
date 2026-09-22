56ea5d50aca7d6e1fad49d3e15302d5f360bf32b
Tony Narlock <tony@git-pull.com>
2026-09-21 19:03:35 -0500

    Scala(test[stream]): Fail when the client ends
    why: A stream that drops the client's end cause
    looks like an unexplained close.
    
    what:
    - Attach, let the client exit, and collect the stream
    - Require the failure to be the client's end cause

---
+++ libtmux-scala-cats/src/test/scala/io/github/libtmux/scaladsl/cats/ObservationSuite.scala
@@ -0,0 +1,42 @@
+package io.github.libtmux.scaladsl.cats
+
+import _root_.cats.effect.IO
+import _root_.cats.effect.unsafe.implicits.global
+import io.github.libtmux.ServerConfig
+import io.github.libtmux.SessionId
+import io.github.libtmux.control.ControlEndedException
+import java.nio.file.Files
+import java.nio.file.attribute.PosixFilePermissions
+import munit.FunSuite
+import scala.concurrent.duration._
+
+final class ObservationSuite extends FunSuite {
+  test("a stream fails when the client ended the subscription") {
+    val directory = Files.createTempDirectory("libtmux-observation")
+    val fake = directory.resolve("tmux")
+    Files.writeString(
+      fake,
+      """#!/bin/sh
+        |printf '%%begin 100 1 0\n%%end 100 1 0\n'
+        |IFS= read -r request
+        |printf '%%begin 101 1 0\n%%end 101 1 0\n'
+        |sleep 0.4
+        |""".stripMargin
+    )
+    Files.setPosixFilePermissions(
+      fake,
+      PosixFilePermissions.fromString("rwx------")
+    )
+    val config = ServerConfig.builder().binary(fake.toString).build()
+    val program = Control
+      .attach[IO](config, new SessionId("$0"))
+      .use(control => control.output(1).use(_.stream.compile.drain))
+    val ran =
+      try program.timeout(3.seconds).attempt.unsafeRunSync()
+      finally {
+        Files.deleteIfExists(fake)
+        Files.deleteIfExists(directory)
+      }
+    assert(ran.left.exists(_.isInstanceOf[ControlEndedException]), ran)
+  }
+}
